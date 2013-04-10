90eafdaba29eb126628c46f4c1a4e6e5458682c7
Sven Stam <sstam@ask-cs.com>
2013-04-10 09:31:01 +0200

    First non-working version

---
+++ askfast.js
@@ -0,0 +1,21 @@
+
+var AskFast = function() {
+}
+
+AskFast.prototype.ask = function(ask, next) {
+    
+}
+
+AskFast.prototype.say = function(say, next) {
+    
+}
+
+AskFast.prototype.redirect = function(redirect, next) {
+    
+}
+
+AskFast.prototype.hangup = function() {
+    
+}
+
+module.exports = AskFast;
