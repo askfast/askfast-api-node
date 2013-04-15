9ae51734708e9677addbba937048b28431d17f25
sven4ask <sstam@ask-cs.com>
2013-04-15 09:34:58 +0200

    First working version

---
+++ examples/question-answer-example.js
@@ -0,0 +1,27 @@
+var http = require('http'),
+    url = require('url'),
+    AskFast = require('askfast');
+
+var server = http.createServer(function (request, response) {
+
+    // Create a new instance of the AskFast object.
+    var askfast = new AskFast(request);
+    var query = url.parse(request.url,true).query;
+    
+    if(query.function=="result") {
+        var answer = query.answer;
+        var say = "OK thanks!";
+        if(answer==2) {
+            say = "That's to bad, try it yourself!";
+        }
+        askfast.say(say);
+    } else {
+        askfast.ask("How do like this example?");
+        askfast.addAnswer("yes", "?function=result&answer=1");
+        askfast.addAnswer("no", "?function=result&answer=2");
+    }
+    // Render out the JSON for AskFast to consume.
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(askfast.finalize());
+
+}).listen(8000); // Listen on port 8000 for requests
---
+++ examples/simple-example.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var AskFast = require('askfast');
+
+var server = http.createServer(function (request, response) {
+
+	// Create a new instance of the TropoWebAPI object.
+	var askfast = new AskFast();
+	askfast.say("Hello, World!");
+
+	// Render out the JSON for Tropo to consume.
+	response.writeHead(200, {'Content-Type': 'application/json'});
+	response.end(askfast.finalize());
+
+}).listen(8000); // Listen on port 8000 for requests.
